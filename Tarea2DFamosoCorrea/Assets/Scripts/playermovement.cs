5f7a76f1a368f3406aa0c1e6db465c42060d5c19
EstebanCcorrea <estebancho8@hotmail.com>
2025-09-24 00:04:41 -0500

    Animacion movimientos basicos

--- Tarea2DFamosoCorrea/Assets/Scripts/playermovement.cs
+++ Tarea2DFamosoCorrea/Assets/Scripts/playermovement.cs
@@ -1,15 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-    
-    public float speed = 5f;
 
-    
+    [SerializeField]public float speed;
+    private Rigidbody2D rb2D;
 
-    
+    private float move;
+
+    public float jumpforce = 4;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundRadius = 0.1f;
+    public LayerMask groundLayer;
+    private Animator anim;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
-        transform.position += Vector3.right * speed * Time.deltaTime;
+        move = Input.GetAxis("Horizontal");
+        rb2D.linearVelocity = new Vector2(move*speed, rb2D.linearVelocity.y);
+
+        if(move !=0 )
+            transform.localScale = new Vector3(Mathf.Sign(move),1,1);
+        if(Input.GetButtonDown("Jump") && isGrounded)
+            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpforce);
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        anim.SetFloat("VerticalVelocity", rb2D.linearVelocity.y);
+        anim.SetBool("IsGrounded", isGrounded);
+
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
     }
 }
