28e57c8c474b084dbb5aacdc4dadc45fe1b400a8
EstebanCcorrea <estebancho8@hotmail.com>
2025-09-25 04:44:36 -0500

    se sube antes de embarrarla

--- Tarea2DFamosoCorrea/Assets/Scripts/GameManager.cs
+++ Tarea2DFamosoCorrea/Assets/Scripts/GameManager.cs
@@ -1,72 +1,94 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public List<Personajes> personajes;
+
     [Header("Contadores globales")]
     public int cerezasRecolectadas = 0;
     public int kiwisRecolectados = 0;
 
     [Header("Tiempos por nivel")]
     public List<float> tiemposNiveles = new List<float>();
+    private float tiempoInicial;
+    private Tiempo tiempoScript;
+
+    private GameObject sigNivObj;
 
     private void Awake()
     {
-        if (GameManager.Instance == null)
-        {
-            GameManager.Instance = this;
-            DontDestroyOnLoad(gameObject); // Mantener entre escenas
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); // Mantener entre escenas
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
-        else
+        else if (Instance != this)
         {
             Destroy(gameObject);
         }
     }
 
-    /// <summary>
-    /// Suma fruta al contador global.
-    /// </summary>
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        sigNivObj = GameObject.FindGameObjectWithTag("SigNiv");
+        if (sigNivObj != null) sigNivObj.SetActive(false);
+
+        tiempoScript = Object.FindFirstObjectByType<Tiempo>();
+        if (tiempoScript != null)
+        {
+            tiempoInicial = tiempoScript.tiemporestante;
+        }
+    }
+
+    private void Update()
+    {
+        if (sigNivObj != null && !sigNivObj.activeSelf)
+        {
+            GameObject[] frutas = GameObject.FindGameObjectsWithTag("Cereza");
+            GameObject[] kiwis = GameObject.FindGameObjectsWithTag("Kiwi");
+
+            if (frutas.Length == 0 && kiwis.Length == 0)
+            {
+                sigNivObj.SetActive(true);
+                Debug.Log("Todas las frutas recolectadas. Activando SigNiv.");
+            }
+        }
+    }
+
     public void SumarFruta(string tipoFruta)
     {
         if (tipoFruta == "Cereza")
         {
             cerezasRecolectadas++;
-            Debug.Log("Cerezas: " + cerezasRecolectadas);
         }
         else if (tipoFruta == "Kiwi")
         {
             kiwisRecolectados++;
-            Debug.Log("Kiwis: " + kiwisRecolectados);
         }
     }
 
-    /// <summary>
-    /// Reinicia los contadores (usado al morir o reiniciar nivel).
-    /// </summary>
     public void ResetCounters()
     {
         cerezasRecolectadas = 0;
         kiwisRecolectados = 0;
-        Debug.Log("Contadores reiniciados.");
+        tiemposNiveles.Clear();
     }
 
-    public void RegistrarTiempoNivel(float tiempoRestante, float tiempoInicial)
+    public void RegistrarTiempoNivel()
     {
-        float tiempoDemorado = tiempoInicial - tiempoRestante;
-        tiemposNiveles.Add(tiempoDemorado);
+        if (tiempoScript != null)
+        {
+            float tiempoRestante = tiempoScript.tiemporestante;
+            float tiempoDemorado = tiempoInicial - tiempoRestante;
+            tiemposNiveles.Add(tiempoDemorado);
+            Debug.Log($"Nivel {tiemposNiveles.Count} completado en {tiempoDemorado} segundos");
+        }
 
-        Debug.Log("Nivel " + tiemposNiveles.Count + " completado en: " + tiempoDemorado + " segundos");
+        int escenaActual = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(escenaActual + 1);
     }
-
 }
